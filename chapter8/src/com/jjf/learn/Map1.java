4b534c38a2b4fb938ce4f377367cd1269d2ee8d8
asus <jjf6013@foxmail.com>
2024-10-31 17:34:11 +0800

    20241031集合学习同步4

--- chapter8/src/com/jjf/learn/Map1.java
+++ chapter8/src/com/jjf/learn/Map1.java
@@ -32,6 +32,50 @@ public class Map1 {
     }
 
     public static void printValues(Map m){
+        System.out.print("Values:");
+        Collection1.print(m.values());
+    }
+
+    public static void print(Map m){
         Collection entries = m.entrySet();
+        Iterator it = entries.iterator();
+        while(it.hasNext()){
+            Map.Entry e = (Map.Entry)it.next();
+            System.out.println("Key = " + e.getKey() + ",Value = " + e.getValue());
+        }
+    }
+
+    public static void test(Map m){
+        fill(m, testData1);
+        fill(m, testData1);
+
+        printKeys(m);
+        printValues(m);
+        print(m);
+
+        String key = testData1[4][0];
+        String value = testData1[4][1];
+
+        System.out.println("m.containsKey(\"" + key + "\"):" + m.containsKey(key));
+        System.out.println("m.get(\"" + key + "\"):" + m.get(key));
+        System.out.println("m.containsValue(\"" + value + "\"):" + m.containsValue(value));
+
+        Map m2 = fill(new TreeMap(), testData2);
+        m.putAll(m2);
+        printKeys(m);
+        m.remove(testData2[0][0]);
+        printKeys(m);
+        m.clear();
+        System.out.println("m.isEmpty():" + m.isEmpty());
+        fill(m, testData1);
+        m.keySet().removeAll(m.keySet());
+        System.out.println("m.isEmpty():" + m.isEmpty());
+    }
+
+    public static void main(String args[]){
+        System.out.println("Testing HashMap");
+        test(new HashMap());
+        System.out.println("Testing TreeMap");
+        test(new TreeMap());
     }
 }
